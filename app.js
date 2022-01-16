87de3f6abb0e45845f380b2f2e1a6ed2491800a3
ludfiwahyu <ludfiwahyuh@gmail.com>
2022-01-17 00:34:24 +0700

    new server deploy

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const app = express()
+const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 
-
 const io = require("socket.io")(server, {
-	cors: {
-		origin: "*",
-		methods: [ "GET", "POST" ]
-	}
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cors());
@@ -16,25 +14,23 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Server Hack Video Call is running");
+  res.send("HackMedical Server video call is running");
 });
 
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("callended");
+    socket.broadcast.emit("callEnded");
   });
 
-  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
   });
 
-  socket.on("answercall", (data) => {
-    io.to(data.to).emit("calledaccepted", data.signal);
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
   });
 });
 
-server.listen(PORT, () => {
-  console.log("Server running on PORT " + PORT);
-});
+server.listen(PORT, () => console.log(`HackMedical Server video call is running on port ${PORT}`));
--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -5,7 +5,8 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 const socket = io('http://localhost:5000');
-// const socket = io('https://video-chat-hackmedical.herokuapp.com/');
+// const socket = io('https://video-chat-hackmedical.herokuapp.com');
+console.log(socket)
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
